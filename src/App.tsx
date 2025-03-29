47e26bd3059809cf9f2da62d162b9b9ffeca03d6
Matthias Malek <matthiasmalek72@gmail.com>
2025-03-29 11:48:07 +0900

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 function App() {
-  return ( 
-
-    <div>
-      <h1> Hello from Tokyo</h1>
+  return (
+    <div
+      className="w-full h-full flex justify-center items-center bg-[url('./public/vite.svg')] bg-cover bg-center bg-no-repeat bg-blend-overlay bg-black/40"
+    >
+      <h1 className="text-6xl font-bold text-center text-white">
+        Welcome to My Awesome App
+      </h1>
     </div>
 
 
